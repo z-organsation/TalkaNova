eafb04bd3c008609d9c6839f2626dcf71a70a6af
imadzakxy <imadzakxy@gmail.com>
2025-10-13 01:42:23 +0100

    server

--- Back-end/serveur.py
+++ Back-end/serveur.py
@@ -2,24 +2,23 @@ import threading
 import socket
 
 
-host = '10.109.187.233' 
-port= 5555
+host = '192.168.53.118' 
+port = 5555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
 server.listen()
 
-clients =[]
-nicknames =[]
+clients = []
+nicknames = []
 
-def broadcast (mesage):
+def broadcast (message):
     for client in clients:
         client.send(message)
 
 
 def handle (client):
-
-    while true:
+    while True:
         try:
             message = client.recv(1024)
             broadcast(message)
@@ -29,3 +28,25 @@ def handle (client):
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client,address = server.accept()
+        print(f"connected {str(address)}")
+
+        client.send('send ur name'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'nickname is {nickname} !')
+        broadcast(f'{nickname} joined the chat !'.encode('ascii'))
+        client.send('connected to serveur !'.encode('ascii'))
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+print("server up !")
+print("server listening ...")
+receive()
